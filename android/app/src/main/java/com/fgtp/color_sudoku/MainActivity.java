bdee0f6a1fa9470e3c2c2bb8d5e21d4f0e3932db
Shyam Mendapara <shyampmendapara@gmail.com>
2025-11-06 11:57:53 +0530

    Integrate Facebook SDK for enhanced social features
    - Added Facebook SDK dependencies in both Android and iOS configurations.
    - Updated AndroidManifest.xml to include Facebook App ID metadata.
    - Initialized Facebook SDK in MainActivity for event logging.
    - Configured iOS AppDelegate to handle Facebook app events and URL schemes.
    - Updated Info.plist to include Facebook app settings and URL types.

--- android/app/src/main/java/com/fgtp/color_sudoku/MainActivity.java
+++ android/app/src/main/java/com/fgtp/color_sudoku/MainActivity.java
@@ -1,6 +1,14 @@
 package com.fgtp.color_sudoku;
 
 import io.flutter.embedding.android.FlutterActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
 }
